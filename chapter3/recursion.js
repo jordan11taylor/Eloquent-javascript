6f209971526b8bf51e2df1cae8886c94446e8079
Jordan Keating <jordan@brakelazy.com>
2016-04-28 10:45:00 -0700

    adds second homework from chapter 3

---
+++ chapter3/recursion.js
@@ -0,0 +1,16 @@
+
+function isEven(n) {
+  if ((n % 10) == 0)
+    return 'even';
+  else if((n % 10) == 1)
+    return 'odd';
+  else
+    return isEven((n % 10) -2);
+};
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
